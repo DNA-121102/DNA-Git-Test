0cccd4162bfeb993381dd2920c4bf1f3b4eb433c
DNA-121102 <danimille88@gmail.com>
2025-01-12 16:06:11 +0700

    pelajaran javascript baru

--- latihan.js
+++ latihan.js
@@ -1,1 +1,16 @@
-alert('Hello world');
+// alert('Hello world');
+// alert('Hy');
+// alert('Bro');
+// var nama=prompt('masukkan nama');
+// alert (nama);
+// var konfir=confirm('kamu yakin?');
+// alert(konfir);
+alert('selamat datang..');
+var lagi=true;
+while (lagi === true){
+    var nama=prompt('masukkan nama:');
+    alert('halo'+nama);
+
+    lagi = confirm('coba lagi');
+}
+alert('terima kasih');
