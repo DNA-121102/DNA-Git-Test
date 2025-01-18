17d6d388deeb0c7dd0fb4fcd95dfd68df87fa8d7
DNA-121102 <danimille88@gmail.com>
2025-01-18 08:30:48 +0700

    Membuat pengulangan bersarang dengan bentuk segitiga siku-siku

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 s='';
-for (var i=0;i<5;i++){
-    for(var j=0;j<i+1;j++){
+var k=5;
+for (var i=0;i<k;i++){
+    for(var j=k;j>i;j--){
        s+='*';
     } 
     
     s+='\n';
 }
 console.log(s);
+// Atau dengan bentuk lain 
+var t='';
+for (var a=5;a>0;a--){
+    for (var b=0;b<a;b++){
+        t+='*'; 
+    }
+    t+='\n';
+}
+console.log(t);
